f3453e2a07577a56aa3b5dcad5e6363a0298bad8
skyshim <skyshim0128@gmail.com>
2024-11-03 01:11:44 +0900

    모고다운 주요기능 추가 -여러 요소 선택 기반 제작 -파일 다운로드 기능 구현

--- 10628 심성우/js/main.js
+++ 10628 심성우/js/main.js
@@ -3,6 +3,8 @@ const yearList = document.querySelector('.yearbox');
 const monthList = document.querySelector('.monthbox');
 const subjectList = document.querySelector('.subjectbox');
 
+const pdfList = document.querySelector('.mogo-list');
+
 const searchBtn = document.querySelector('.search');
 
 let gradeN; //몇학년?
@@ -22,6 +24,34 @@ let subject;
 let subjectOn; 
 
 let nameCode;
+
+function sub_to_str(text) {
+    switch (text) {
+        case "kor": return "국어"
+        case "mat": return "수학"
+        case "eng": return "영어"
+        case "soc": return "사회"
+        case "sci": return "과학"
+        case "his": return "한국사"
+    }
+}
+
+function del_zero(num) {
+    switch (num) {
+        case "03": return "3"
+        case "06": return "6"
+        case "09": return "9"
+    }
+}
+
+function download_file(name) {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data/' + name + '.pdf');
+    element.setAttribute('download', name + '.pdf');
+    element.click();
+    // element.remove();  //element 를 삭제해야 하는지 확인 필요
+    console.log(element)
+}
 
 gradeList.addEventListener('click', (e)=> {
 
@@ -90,10 +120,47 @@ searchBtn.addEventListener('click', (e)=> {
         return
     }
     else {
-        console.log(nameCode)
-        window.open('./pdfViewer.html','_blank');
+        //안에 요소 초기화
+        while (pdfList.firstChild) {
+            pdfList.removeChild(pdfList.firstChild);
+        }
 
-        localStorage.setItem("nameCode",nameCode);
+        //초기화 후 요소 추가
+        console.log(nameCode)
+        var rightMogo = document.createElement('div');
+        rightMogo.className = "pdf-file";
+        
+        var mogo_button_con = document.createElement('div');
+        mogo_button_con.className = "mogo-btn-container";
+
+        var mogo_title = document.createElement('div');
+        mogo_title.textContent = yearN + "년 " + del_zero(monthN) + "월 고" + gradeN + " 모의고사 - " + sub_to_str(subjectN) + " 영역"; // 예) 2023년 9월 고1 모의고사 - 국어 영역
+        rightMogo.appendChild(mogo_title);
+        
+        var mogo_open = document.createElement('div');
+        mogo_open.textContent = "문제보기";
+        mogo_open.className = "mogo-open-btn"; // CSS 클래스 추가 (선택 사항)
+        mogo_button_con.appendChild(mogo_open);
+
+        var mogo_download = document.createElement('div');
+        mogo_download.textContent = "다운로드";
+        mogo_download.className = "mogo-download-btn"; // CSS 클래스 추가 (선택 사항)
+        mogo_button_con.appendChild(mogo_download);
+
+        rightMogo.appendChild(mogo_button_con);
+
+        pdfList.appendChild(rightMogo)
     }
 })
 
+pdfList.addEventListener('click', (e)=> {
+    
+    if (e.target.matches('.mogo-open-btn')) {
+        window.open('./pdfViewer.html','_blank');
+        localStorage.setItem("nameCode", nameCode);
+    }
+
+    if (e.target.matches('.mogo-download-btn')) {
+        download_file(nameCode);
+    }   
+})
